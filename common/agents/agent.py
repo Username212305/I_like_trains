72a9e612b4a63b316570d3d2ff638176de334445
Isgrammir <theobernetcodoni@gmail.com>
2025-05-06 16:06:46 +0200

    agent47 fais des trucs ,code s execute en entier

--- common/agents/agent.py
+++ common/agents/agent.py
@@ -34,10 +34,10 @@ class Agent(BaseAgent):
             case [0,-1]:
                 self.opp_cur_dir = "up"
         self.opp_len = int(len(self.autre["wagons"]))
-        self.opponent_loc = set(self.autre["wagons"])
+        self.opponent_loc = (self.autre["wagons"])
         self.opponent_head = (self.autre["position"][0]//self.cell_size,self.autre["position"][1]//self.cell_size) 
         """ info sur delivery zone"""    
-        zone_loc = [tuple(self.delivery_zone["position"])]
+        self.zone_loc = [(self.delivery_zone["position"][0]//self.cell_size , self.delivery_zone["position"][1]//self.cell_size)] # zone_loc = [x,y] case de la zone de livraison
         znch = self.delivery_zone["height"]//20 #zone_nb_case_haut, combien de cases de haut fait la zone
         zncl = self.delivery_zone["width"]//20 #zone_nb_case_large, idem de large
         
@@ -48,19 +48,16 @@ class Agent(BaseAgent):
                         print()
                     case _:
                         for x in range(1,zncl):
-                            zone_loc.append((zone_loc[0][0]+x*20,zone_loc[0][1]))
+                            self.zone_loc.append((self.zone_loc[0][0]+x,self.zone_loc[0][1]))
             case _:
                 match zncl:
                     case 1:
                         for y in range(1,znch):
-                            zone_loc.append((zone_loc[0][0],zone_loc[0][1]+y*20))
+                            self.zone_loc.append((self.zone_loc[0][0],self.zone_loc[0][1]+y))
                     case _:
                         for y in range(1,znch):
                             for x in range(1,zncl):
-                                zone_loc.append((zone_loc[0][0] + x*20,zone_loc[0][1] + y*20)) # à voir si le dernier cas suffit pas, histoire de faire propre
-        self.zone_loc = []
-        for i in zone_loc:
-            self.zone_loc.append((i[0]//self.cell_size,i[1]//self.cell_size)) 
+                                self.zone_loc.append((self.zone_loc[0][0] + x,self.zone_loc[0][1] + y)) # à voir si le dernier cas suffit pas, histoire de faire propre
         """ info sur passagers"""
         passagers = self.passengers
         passen1_loc = (passagers[0]["position"][0]//self.cell_size,passagers[0]["position"][1]//self.cell_size)
@@ -80,8 +77,8 @@ class Agent(BaseAgent):
             case [0,-1]:
                 self.cur_dir = "up"
         self.our_len = int(len(self.train["wagons"]))
-        self.our_loc = set(self.train["wagons"])
-        self.our_head = (self.train["position"][0]//self.cell_size,self.train["position"][1]//self.cell_size)
+        self.our_loc = (self.train["wagons"])
+        self.our_head = (self.train["position"][0]//self.cell_size , self.train["position"][1]//self.cell_size)
         """ Les distances """
         d_passen1 = abs(passen1_loc[0] - self.our_head[0] + passen1_loc[1] - self.our_head[1])
         d_passen2 = abs(passen2_loc[0] - self.our_head[0] + passen2_loc[1] - self.our_head[1])
@@ -91,6 +88,7 @@ class Agent(BaseAgent):
         print(d_zone)
         """d_zone elaborée"""
         d_zmin = d_zone
+        self.zone_min = self.zone_loc[0] # zone la plus proche de nous (initialement, c'est la zone d'origine)
         for i,c in enumerate(self.zone_loc):
             d = abs(c[0] - self.our_head[0]) + abs(c[1] - self.our_head[1])
             if d < d_zmin:
@@ -114,16 +112,16 @@ class Agent(BaseAgent):
         # In-zone handler, in case we are in the zone and still need to let passengers:
         # We call a variable "d" to gain space: its the distance on (x, y) between the first corner point
         # of the zone and us
-        d = (self.our_head[0] - zone_loc[0][0], self.our_loc[1] - zone_loc[0][1])
+        d = (self.our_head[0] - self.zone_loc[0][0], self.our_head[1] - self.zone_loc[0][1])
         # This variable is True if the train is in the zone but NOT in a corner (e.t. not in zone_loc)
         Train_in_middle_zone = True if (d[0] > 0 and d[0]-zncl < 0) and (d[1] > 0 and d[1] - znch < 0) else False
-        if self.our_len != 0 and (self.our_head in zone_loc or Train_in_middle_zone):
-            for i in zone_loc:
+        if self.our_len != 0 and (self.our_head in self.zone_loc or Train_in_middle_zone):
+            for i in self.zone_loc:
                 if i == self.our_head or i in self.our_loc or i in self.opponent_loc:
                     continue
                 self.target = i
             # if all corner points are unavailable:
-            return zone_loc[0]
+            self.target = self.zone_loc[0]
 
         # Determinig next target:  
         else:
@@ -135,6 +133,8 @@ class Agent(BaseAgent):
             if weight_passen1 >= weight_passen2:
                 if weight_passen1 >= weight_zone:
                     self.target = passen1_loc
+                else:
+                    self.target = self.zone_min
             elif weight_passen2 >= weight_zone:
                 self.target = passen2_loc
             else:
